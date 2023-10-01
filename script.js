026cd7d1b815695befdbb0c59f827ceb1484c633
msnadeem96 <m.saqib.hassan96@gmail.com>
2023-10-01 23:40:49 +0500

    fixes for mobile

--- script.js
+++ script.js
@@ -1,37 +1,77 @@
 (() => {
+    const RIGHT_LIMIT = 100
+
     let trackPos = 0
     let isMouseDown = false
+    let previousTouch
 
     const track = document.getElementById('track')
     const images = document.getElementsByClassName('image')
 
-    window.addEventListener('mousedown', e => {
+    const mouseDown = e => {
         isMouseDown = true
-    })
+    }
 
-    window.addEventListener('mouseup', e => {
+    const mouseUp = e => {
         isMouseDown = false
-    })
-
-    window.addEventListener('mousemove', e => {
-        if (isMouseDown === false)
-            return false;
-
+    }
+    const moveTrack = e => {
         const change = -e.movementX / window.screen.availWidth * 200
 
         trackPos += change
 
-        trackPos = trackPos > 100 ? 100 : trackPos
+        trackPos = trackPos > RIGHT_LIMIT ? RIGHT_LIMIT : trackPos
         trackPos = trackPos < 0 ? 0 : trackPos
 
         const translate = `translate(-${trackPos}%, -50%)`
 
         track.style.transform = translate
 
-        console.log(trackPos)
         for (let image of images) {
             image.style.objectPosition = trackPos + "% 50%"
         }
-    })
+    }
+
+
+    const mouseMove = e => {
+        if (isMouseDown === false)
+            return false;
+
+        moveTrack(e)
+    }
+
+
+    const touchMove = e => {
+
+        const touch = e.touches[0];
+
+        if (!previousTouch) {
+            previousTouch = touch;
+        };
+
+
+        e.movementX = 0.1
+        e.movementY = 0.1
+
+        // be aware that these only store the movement of the first touch in the touches array
+        e.movementX *= touch.pageX - previousTouch.pageX;
+        e.movementY *= touch.pageY - previousTouch.pageY;
+
+        moveTrack(e)
+    }
+
+    const touchStart = e => {
+        previousTouch = e.touches[0];
+    }
+    const touchEnd = e => {
+        previousTouch = null;
+    }
+
+    window.addEventListener('mousedown', mouseDown)
+    window.addEventListener('mouseup', mouseUp)
+    window.addEventListener('mousemove', mouseMove)
+    window.addEventListener('touchstart', touchStart)
+    window.addEventListener('touchEnd', touchEnd)
+    window.addEventListener('touchmove', touchMove)
 })()
 
